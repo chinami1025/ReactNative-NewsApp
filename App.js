1c7260b3f9cc1b5d02f4518a5ed9ccde964cc99e
chinami1025 <toda.chinami@gmail.com>
2023-02-01 12:07:08 +0900

    NewsAPIの取得

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import NewsKizi from "./components/NewsKizi";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URI = `https://newsapi.org/v2/top-headlines?country=jp&category=entertainment&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    const response = await axios.get(URI);
+    setNews(response.data.articles);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <View style={styles.moziBox}>
-          <Text>
-            これは可愛い犬の画像です。私は猫を2匹飼っていますが犬も飼いたいです。
-          </Text>
-          <Text style={styles.subText}>テム</Text>
-        </View>
-        <View style={styles.gazoBox}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: "https://picsum.photos/id/237/200/300",
-            }}
+      <FlatList
+        data={news}
+        renderItem={({ item }) => (
+          <NewsKizi
+            imageuri={item.urlToImage}
+            title={item.title}
+            subtext={item.publishedAt}
           />
-        </View>
-      </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -28,36 +39,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  box: {
-    height: 100,
-    width: "100%",
-    borderColor: "lightblue",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-
-  moziBox: {
-    flex: 1,
-    backgroundColor: "steelblue",
-    padding: 16,
-    justifyContent: "space-between",
-  },
-
-  gazoBox: {
-    width: 100,
-    backgroundColor: "powderblue",
-  },
-
-  Text: {
-    fontSize: 16,
-  },
-
-  subText: {
-    fontSize: 12,
-    color: "red",
   },
 });
